f064704226dc900dd32d201b47f2ff7c9f6f86da
Jorge Peguero <doiany.1412@gmail.com>
2026-08-19 15:24:16 -0400

    feat: add CIPAC EDA notebook and update data loading utilities

--- utils/data.py
+++ utils/data.py
@@ -1,5 +1,10 @@
+import os
 import pandas as pd
 import streamlit as st
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
 
 
 def validate_required_columns(
@@ -21,4 +26,34 @@ def load_data() -> pd.DataFrame:
         lambda x: 'Documentary' if isinstance(x, str)
         and 'documentary' in x.lower() else 'Non-Documentary'
     )
+    return df
+
+
+@st.cache_data
+def load_cipac_data() -> pd.DataFrame:
+    """Load all CIPAC resolutions from Supabase."""
+    engine = create_engine(os.getenv('SUPABASE_DB_URL') or '')
+    df = pd.read_sql("""
+        SELECT
+            resolution_number,
+            year,
+            incentive_article,
+            resolution_type,
+            investor_name,
+            investor_rnc,
+            producer_rnc,
+            film_title,
+            local_company,
+            pur_number,
+            cpnd_number,
+            request_date,
+            resolution_date,
+            validated_expenses_dop,
+            tax_credit_dop,
+            tax_credit_pct,
+            total_budget_approved,
+            total_budget_executed
+        FROM cipac_resolutions
+        ORDER BY year, resolution_number
+    """, engine)
     return df
